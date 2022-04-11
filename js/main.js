2b852114e8864f14a536b47fef78fdeed3bebf36
Catalin Siegling <csiegling@gmail.com>
2022-04-11 13:35:31 -0500

    added entiree APOD to git

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch() {
+    const choice = document.querySelector('input').value
+    console.log(choice)
+
+    const url = `https://api.nasa.gov/planetary/apod?api_key=zU71SV2z8UAS2tpSRxtx9Ii4giGUAk6QIufK4bCn&date=${choice}`
+
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data)
+            if (data.media_type === 'image') {
+                document.querySelector('#image1').src = data.hdurl
+            } else if (data.media_type === 'video') {
+                document.querySelector('iframe').src = data.url
+            }
+
+            document.querySelector('#title_id').innerText = data.title
+            document.querySelector('#explanation1').innerText = data.explanation
+            document.querySelector('#date_id').innerText = data.date
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+}
